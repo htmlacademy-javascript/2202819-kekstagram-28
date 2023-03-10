870418ac6a75e082bd3eaf50f30b0df3392f0d68
Эдуард <krav_4@mail.ru>
2023-03-10 13:00:28 +0300

    Создал модуль для отрисовки миниатюр

---
+++ js/miniature.js
@@ -0,0 +1,17 @@
+import {getPhotos} from './data.js';
+
+const photosContainer = document.querySelector('.pictures');
+const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const miniatures = getPhotos();
+const miniaturesListFragment = document.createDocumentFragment();
+
+miniatures.forEach(({url, likes, comments}) => {
+  const miniature = photoTemplate.cloneNode(true);
+  miniature.querySelector('.picture__img').src = url;
+  miniature.querySelector('.picture__likes').textContent = likes;
+  miniature.querySelector('.picture__comments').textContent = comments.length;
+  miniaturesListFragment.appendChild(miniature);
+});
+
+photosContainer.appendChild(miniaturesListFragment);
