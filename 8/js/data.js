8a010801eebdaf4e3eb79f08626a4a1a48afa5f9
Keks <keks@htmlacademy.ru>
2023-03-19 10:17:37 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/data.js
@@ -0,0 +1,56 @@
+/*Генерация данных*/
+
+import {getRandomInteger, getRandomArrayElement, createIdGenerator} from './util.js';
+
+const PHOTOS_COUNT = 25;
+const AVATARS_COUNT = 6;
+const CommentsCount = {MIN: 1, MAX: 2};
+const LikesCount = {MIN: 15, MAX: 200};
+
+const NAMES = [
+  'Александр',
+  'Ольга',
+  'Андрей',
+  'Анна',
+  'Николай',
+  'Эдуард',
+];
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCRIPTIONS = [
+  'Как вам фотка?',
+  'И пусть весь мир подождет!',
+  'Не судите строго, я начинающий фотограф',
+  'Фото дня',
+  'Зацени',
+  'Красота в глазах смотрящего',
+];
+
+const generateCommentId = createIdGenerator();
+
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${ getRandomInteger(1, AVATARS_COUNT) }.svg`,
+  message: getRandomArrayElement(COMMENTS),
+  name: getRandomArrayElement(NAMES),
+});
+
+const createPhoto = (index) => ({
+  id: index,
+  url: `photos/${ index }.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(LikesCount.MIN, LikesCount.MAX),
+  comments: Array.from({length: getRandomInteger(CommentsCount.MIN, CommentsCount.MAX)}, createComment),
+});
+
+const getPhotos = () => Array.from({length: PHOTOS_COUNT}, (_, photoIndex) => createPhoto(photoIndex + 1));
+
+export {getPhotos};
---
+++ 8/js/miniature.js
@@ -0,0 +1,31 @@
+/*Отрисовка миниатюр*/
+
+import {getPhotos} from './data.js';
+
+const photosContainer = document.querySelector('.pictures');
+const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderMiniature = (({url, description, likes, comments, id}) => {
+  const miniature = photoTemplate.cloneNode(true);
+  miniature.querySelector('.picture__img').src = url;
+  miniature.querySelector('.picture__img').alt = description;
+  miniature.querySelector('.picture__likes').textContent = likes;
+  miniature.querySelector('.picture__comments').textContent = comments.length;
+  miniature.dataset.id = id;
+
+  return miniature;
+});
+
+const createMiniaturesList = (miniatures) => {
+  const miniaturesListFragment = document.createDocumentFragment();
+
+  miniatures.forEach((miniature) => {
+    miniaturesListFragment.append(renderMiniature(miniature));
+  });
+  photosContainer.append(miniaturesListFragment);
+};
+
+const renderMiniatures = getPhotos();
+createMiniaturesList(renderMiniatures);
+
+export {renderMiniatures, photosContainer};
