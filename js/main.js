f660b51598d0277079e841b0441c13b28e86201e
Эдуард <krav_4@mail.ru>
2023-04-05 13:30:08 +0300

    Корректировка

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 import {getData} from './api.js';
 import {showAlert} from './util.js';
 import {createGallery} from './gallery.js';
-import {showFilters, setFilterClickHandler} from './filter.js';
+import {showFilters, setOnFilterClick} from './filter.js';
 import {closePictureUploadForm, onPictureUploadFormSubmit} from './form.js';
 
 getData()
   .then((pictures) => {
     createGallery(pictures);
     showFilters();
-    setFilterClickHandler(pictures);
+    setOnFilterClick(pictures);
   })
   .catch((err) => {
     showAlert(err.message);
